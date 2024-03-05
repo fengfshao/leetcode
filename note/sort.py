336561add0ff575e86100c0d27d006e0b7f23ab0
berry <berryIIInew@gmail.com>
2024-03-05 14:36:08 +0800

    快排

---
+++ note/sort.py
@@ -0,0 +1,33 @@
+from typing import List
+
+def quicksort(nums): 
+    quicksort0(nums,0,len(nums)-1)    
+    
+def quicksort0(nums:List[int],l:int,r:int):
+    if(l<r):
+        q=partition(nums,l,r)
+        quicksort0(nums,l,q-1)
+        quicksort0(nums,q+1,r)
+
+def partition(nums:List[int],l:int,r:int)->int:
+    # nums[l+1,i]<=base , nums[j,r]>base
+    base=nums[l]
+    i=l
+    j=r+1
+    while(i<j-1):
+        if(nums[i+1]<=base):
+            i+=1
+        else:
+            tmp=nums[j-1]
+            nums[j-1]=nums[i+1]
+            nums[i+1]=tmp
+            j-=1
+    nums[l]=nums[i]
+    nums[i]=base
+    return i
+
+nums=[3,7,2,5,1,4,0,12,9,4]            
+quicksort(nums)
+print(nums)
+        
+        
