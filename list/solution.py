7247cc72c1be440cef5a05beb44ca62858fa63cc
tanpopo <tanpopo82456@gmail.com>
2024-05-30 22:49:44 +0800

    list

--- list/solution.py
+++ list/solution.py
@@ -3,6 +3,197 @@ from typing import List
 import queue
 from collections import deque
 import heapq
+import random
+
+'''
+622. Design Circular Queue
+使用数组实现的循环队列
+'''
+class MyCircularQueue:
+
+     def __init__(self, k: int):        
+          self.M=k+1
+          self.data=[0]*self.M
+          self.front=0
+          self.rear=0
+
+     def enQueue(self, value: int) -> bool:
+          if(self.isFull()):
+               return False
+          self.rear=(self.rear+1)%self.M
+          self.data[self.rear]=value
+          return True
+
+     def deQueue(self) -> bool:
+          if(self.isEmpty()):
+               return False
+          self.front=(self.front+1)%self.M 
+          return True
+     
+     def Front(self) -> int:
+          if(self.isEmpty()):
+               return -1
+          return self.data[(self.front+1)%self.M]
+          
+     def Rear(self) -> int:
+          if(self.isEmpty()):
+               return -1
+          return self.data[self.rear]
+
+     def isEmpty(self) -> bool:
+          return self.front==self.rear 
+
+     def isFull(self) -> bool:
+          return (self.rear+1)%self.M==self.front
+
+'''
+641. Design Circular Deque
+使用带头结点的双链表维护双端队列,注意维护好head和tail指针
+'''
+class MyCircularDeque:
+
+     def __init__(self, k: int):
+          self.cap=k
+          self.size=0
+          dummy=DNode(-1,-1)
+          self.head=dummy
+          self.tail=dummy
+
+     def insertFront(self, value: int) -> bool:
+          if(self.isFull()):
+               return False
+          node=DNode(-1,value)
+          node.next=self.head.next
+          node.prev=self.head
+          if(node.next):
+               node.next.prev=node
+          self.head.next=node
+          if(self.size==0):
+               self.tail=node
+          self.size+=1 
+          return True
+
+     def insertLast(self, value: int) -> bool:
+          if(self.isFull()):
+               return False
+          node=DNode(-1,value)
+          node.prev=self.tail
+          self.tail.next=node
+          self.tail=node
+          self.size+=1
+          return True
+
+     def deleteFront(self) -> bool:
+          if(self.isEmpty()):
+               return False
+          node=self.head.next
+          node.prev=None
+          self.head.next=node.next
+          if(node.next):
+               node.next.prev=self.head
+          node.next=None
+          self.size-=1
+          if(self.size==0):
+               self.tail=self.head
+          return True
+
+     def deleteLast(self) -> bool:
+          if(self.isEmpty()):
+               return False
+          node=self.tail
+          self.tail=node.prev
+          node.prev.next=None
+          node.prev=None
+          self.size-=1
+          return True
+
+     def getFront(self) -> int:
+          if(self.isEmpty()):
+               return -1
+          return self.head.next.val   
+
+     def getRear(self) -> int:
+          if(self.isEmpty()):
+               return -1
+          return self.tail.val        
+
+     def isEmpty(self) -> bool:
+          return self.size==0
+
+     def isFull(self) -> bool:
+          return self.size>=self.cap
+
+'''
+705. Design HashSet
+简单的链地址实现
+'''
+class MyHashSet:
+
+     def __init__(self):
+          self.size=1024
+          self.buckets=[[] for _ in range(self.size)] 
+     
+     def __hash(self,key):
+          return key%self.size
+
+     def add(self, key: int) -> None:
+          index = self.__hash(key)
+          if(key not in self.buckets[index]):
+              self.buckets[index].append(key)
+
+     def remove(self, key: int) -> None:
+          index = self.__hash(key)
+          if(key in self.buckets[index]):
+               self.buckets[index].remove(key)
+
+     def contains(self, key: int) -> bool:
+          return key in self.buckets[self.__hash(key)]
+
+'''
+706. Design HashMap
+'''
+class MyHashMap:
+
+     def __init__(self):
+          self.size=1024
+          self.buckets=[[] for _ in range(self.size)] 
+     
+     def __hash(self,key):
+          return key%self.size
+
+     def put(self, key: int, value: int) -> None:
+          index = self.__hash(key)
+          list=self.buckets[index]
+          for i in range(len(list)):
+               if(list[i][0]==key):
+                    list[i][1]=value
+                    return
+          list.append([key,value])
+
+     def get(self, key: int) -> int:
+          index = self.__hash(key)
+          list=self.buckets[index]
+          for i in range(len(list)):
+               if(list[i][0]==key):
+                    return list[i][1]
+          return -1
+
+     def remove(self, key: int) -> None:
+          index = self.__hash(key)
+          list=self.buckets[index]
+          idx=-1
+          for i in range(len(list)):
+               if(list[i][0]==key):
+                    idx=i
+          if(idx>=0):
+               list.pop(idx)
+
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
 
 class ListNode:
      def __init__(self, val=0, next=None):
@@ -603,21 +794,368 @@ class Solution:
                slow=slow.next
           return slow
 
+     '''
+     147. Insertion Sort List
+     题目有点问题,应该用双链表
+     '''
+     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          pass
+
+     '''
+     148. Sort List
+     对链表进行排序 #TODO 实现不对
+     '''
+     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          dummy=ListNode(-1)
+          last=dummy
+          start=head
+          while(start):
+               print(start.val)
+               min=2147483647
+               minNode=None
+               prev=None
+               prevMin=None
+               cur=start
+               while(cur):
+                    if(cur.val<min):
+                        min=cur.val
+                        minNode=cur
+                        prevMin=prev
+                    prev=cur
+                    cur=cur.next
+               if(prevMin):
+                    prevMin.next=prevMin.next.next
+               last.next=minNode
+               last=last.next
+               start=start.next
+          return dummy.next
+     '''
+     160. Intersection of Two Linked Lists
+     先得出两个链表的长度,对齐后开始验证是否某个结点开始链表的结点相同
+     '''
+     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+          lenA,curA=0,headA
+          while(curA):
+               curA=curA.next
+               lenA+=1
+          lenB,curB=0,headB
+          while(curB):
+               curB=curB.next
+               lenB+=1
+          f1,f2=headA,headB
+          n=lenA-lenB
+          if(lenA<lenB):
+               f1,f2=headB,headA
+               n=lenB-lenA
+          for i in range(n):
+               f1=f1.next
+          while(f1):
+               if(f1==f2):
+                    return f1
+               f1=f1.next
+               f2=f2.next
+          return None
+     
+     '''
+     203. Remove Linked List Elements
+     '''     
+     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+          dummy=ListNode(-1)
+          dummy.next=head
+          cur=dummy
+          while(cur.next):
+               if(cur.next.val==val):
+                    cur.next=cur.next.next
+               else:
+                    cur=cur.next
+          return dummy.next
+
+     '''
+     206. Reverse Linked List
+     '''
+     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          last=None
+          cur=head
+          while(cur):
+               next=cur.next
+               cur.next=last
+               last=cur
+               cur=next
+          return last
+
+     '''
+     234. Palindrome Linked List
+     将后半部分倒序后,即可对比,最后可以再reversePart一下把链表恢复
+     ''' 
+     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+          middle=self.middle(head)
+          l=self.reversePart(middle,None)
+          cur=head
+          while(cur!=middle):
+               if(cur.val!=l.val):
+                    return False
+               cur=cur.next
+               l=l.next
+          return True
+     
+     '''
+     237. Delete Node in a Linked List
+     '''
+     def deleteNode(self, node):
+          node.val=node.next.val
+          node.next = node.next.next
+
+     '''
+     328. Odd Even Linked List
+     '''
+     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          cur=head
+          prev=None
+          dummy=ListNode(-1)
+          last=dummy
+          while(cur and cur.next):
+               last.next=cur.next
+               last=last.next
+               prev=cur
+               cur.next=cur.next.next
+               cur=cur.next
+          last.next=None
+          if(cur):
+               cur.next=dummy.next
+          else:
+               prev.next=dummy.next
+          return head
+
+     '''
+     430. Flatten a Multilevel Doubly Linked List
+     '''
+     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+          def flatten0(head):
+               next=head.next
+               child=head.child
+               head.child=None
+               last=head
+               if(child):
+                    last.next=child
+                    child.prev=last
+                    last=flatten0(child)
+               if(next):
+                    last.next=next
+                    next.prev=last
+                    last=flatten0(next)
+               return last
+          
+          flatten0(head)
+          return head
+     
+     '''
+     445. Add Two Numbers II
+     因为要从个位对齐开始加,这里使用栈
+     '''
+     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+          stk1,stk2=[],[]
+          while(l1):
+               stk1.append(l1.val)
+               l1=l1.next
+          while(l2):
+               stk2.append(l2.val)
+               l2=l2.next
+          extra=0
+          last=None
+          while(stk1 or stk2):
+               cur=extra
+               if(stk1):
+                    cur+=stk1.pop()
+               if(stk2):
+                    cur+=stk2.pop()
+               extra=cur//10
+               cur=cur%10
+               newNode=ListNode(cur)
+               newNode.next=last
+               last=newNode
+          if(extra>0):
+               newNode=ListNode(extra)
+               newNode.next=last
+               last=newNode
+          return last
+     
+     '''
+     725. Split Linked List in Parts
+     '''       
+     def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+          cur=head
+          n=0
+          while(cur):
+               cur=cur.next
+               n+=1
+          lens=[]
+          splitSize=n//k
+          for i in range(k):
+               if(i<(n%k)):
+                    lens.append(splitSize+1)
+               else:
+                    lens.append(splitSize)
+          cur=head
+          dummys=[]
+          for i in range(k):
+               dummys.append(ListNode(-1))
+               last=dummys[i]
+               for _ in range(lens[i]):
+                    last.next=cur
+                    last=last.next
+                    cur=cur.next
+               last.next=None
+          for i in range(k):
+               dummys[i]=dummys[i].next
+          return dummys
+     
+     '''
+     817. Linked List Components
+     '''
+     #TODO
+     def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+          pass
+
+     '''
+     876. Middle of the Linked List
+     '''
+     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          slow=head
+          fast=head
+          while(fast and fast.next):
+               fast=fast.next.next
+               slow=slow.next
+          return slow 
+     
+     '''
+     1019. Next Greater Node In Linked List
+     使用单调栈解决这类问题
+     '''
+     def nextLargerNodes(self, head: Optional[ListNode]) -> List[int]:
+          stk=[]
+          resList=[]
+          cur=head
+          idx=0
+          while(cur):
+               while(len(stk)>0 and stk[-1][1]<cur.val):
+                    pair=stk.pop()
+                    # pair[0]的nextGreater是cur
+                    resList.append([pair[0],cur.val])
+               stk.append([idx,cur.val])
+               cur=cur.next
+               idx+=1
+          # 把resList中的解按索引组织
+          res=[0]*idx
+          for pair in resList:
+               res[pair[0]]=pair[1]
+          return res
+
+     '''
+     1171. Remove Zero Sum Consecutive Nodes from Linked List
+     '''
+     def removeZeroSumSublistsV0(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          front = ListNode(0, head)
+          start = front
+
+          while start is not None:
+               prefix_sum = 0
+               end = start.next
+
+               while end is not None:
+                    # Add end's value to the prefix sum
+                    prefix_sum += end.val
+                    # Delete zero sum consecutive sequence 
+                    # by setting node before sequence to node after
+                    if prefix_sum == 0:
+                         start.next = end.next
+                    end = end.next
+
+               start = start.next
+
+          return front.next
+     
+     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+          front = ListNode(0, head)
+          current = front
+          prefix_sum = 0
+          prefix_sum_to_node = {0: front}
+
+          while current is not None:
+               prefix_sum += current.val
+               prefix_sum_to_node[prefix_sum] = current
+               current = current.next
+
+          # Reset prefix sum and current
+          prefix_sum = 0
+          current = front
+
+          while current is not None:
+               prefix_sum += current.val
+               if(prefix_sum in prefix_sum_to_node):
+                    current.next = prefix_sum_to_node[prefix_sum].next
+               current = current.next
+
+          return front.next
+
+     def getDecimalValue(self, head: ListNode) -> int:
+          answer = 0
+          while head: 
+               answer = 2*answer + head.val 
+               head = head.next 
+          return answer
+
+     '''
+     1367. Linked List in Binary Tree
+     使用层序遍历尝试不同的起点,如果知道二叉树高度可以剪支
+     ''' 
+     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+          def isSubPathFromRoot(head: Optional[ListNode], root: Optional[TreeNode]):
+               if(not head): return True
+               if(not root or head.val!=root.val): return False
+               return isSubPathFromRoot(head.next,root.left) or isSubPathFromRoot(head.next,root.right)
+                
+          q=deque()
+          q.append(root)
+          while(q):
+               cur=q.popleft()
+               if(cur.val==head.val and isSubPathFromRoot(head,cur)):
+                    return True
+               if(cur.left):  q.append(cur.left)
+               if(cur.right): q.append(cur.right)
+          return False
+              
+'''
+水塘抽样算法,k=1
+https://www.jianshu.com/p/337997243eea/
+https://blog.csdn.net/Wyf_Fj/article/details/126754116
+'''
+class SolutionRandom:
+     def __init__(self, head: Optional[ListNode]):
+          self.head=head
+     
+     def getRandom(self) -> int:
+          res=self.head.val
+          i=2
+          cur=self.head.next
+          while(cur):
+               if(random.random()<1/i):
+                    res=cur.val
+               cur=cur.next
+               i+=1
+          return res
+
+
+
 sol=Solution()
 l=ListNode(1,ListNode(2,ListNode(3)))
 r=ListNode(4)
 e=ListNode(5)
 #r.next=e
 l.next.next.next=r
-sol.reorderList(l)
+#sol.reorderList(l)
+l.print()
+l2=sol.oddEvenList(l)
+l2.print()
 
-
-cache=LRUCache(2)
-cache.put(1,1)
-cache.put(2,2)
-cache.get(1)
-print(cache.tail.val)
-print(cache.head.next.val)
-cache.put(3,3)
-print(cache.get(2))
-print(cache.get(3))
+list=mkList([2,1,5])
+print(sol.nextLargerNodes(list))
